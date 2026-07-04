e054404f7db34c2b22b13eb2a0b1c6936a2cd320
Chinna1222 <chinnareddygangam1222@gmail.com>
2026-07-04 20:48:26 +0530

    added my first file

---
+++ Chinna/AddTwoNum.java
@@ -0,0 +1,8 @@
+public class AddTwoNum {
+    public static void main(String args[]){
+        int c = 22;
+        int s = 19;
+        int result = c+s;
+        System.out.println("The sum of two numbers is: " + result);
+    }
+}
