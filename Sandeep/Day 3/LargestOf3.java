e97134aef085412965bd4f54add79c410fa2999a
Gadenaboi-Sandeep <gadenaboisandeep2@gmail.com>
2026-07-06 11:03:30 +0530

    Day 3 Begins

---
+++ Sandeep/Day 3/LargestOf3.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class LargestOf3 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter 3 Numbers with spaces: ");
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+        
+        int largest=a;
+
+        if (a > b && a > c) {
+            largest=a;
+        } else if (b > a && b > c) {
+            largest=b;
+        } else {
+            largest=c;
+        }
+
+        System.out.println(largest+" is the Greater Number.");
+        sc.close();
+    }
+}
---
+++ Sandeep/Day 3/PoNeZe.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class PoNeZe{
+public static void main(String[] args) {
+    Scanner sc=new Scanner(System.in);
+    System.out.println("Enter the Number: ");
+    int num=sc.nextInt();
+
+    if(num==0){
+        System.out.println("Oops! It's a Zero.");
+    }
+    else if(num>0){
+        System.out.println(num+" is a Positive Number.");
+    }
+    else{
+        System.out.println(num+" is a Negative Number.");
+    }
+
+    sc.close();
+    }
+}
