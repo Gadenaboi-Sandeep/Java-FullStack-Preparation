a5736f06fe9ba45f6fc75c656714fb5ca4551bc6
Gadenaboi-Sandeep <gadenaboisandeep2@gmail.com>
2026-07-06 16:43:13 +0530

    Done with Conditinonal Statements

---
+++ Sandeep/Day 3/ElectricityBill.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class ElectricityBill {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+
+        System.out.println("Enter the Units:");
+        int units=sc.nextInt();
+        int bill=0;
+
+        if(units>0 && units<=100){
+            bill=units*2;
+        }
+        else if(units>101 && units<=300){
+            bill=units*5;
+        }
+        else if(units>301){
+            bill=units*7;
+        }
+        System.out.println("Total Bill: "+bill);
+
+        sc.close();
+    }
+}
