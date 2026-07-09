56ca2009b26e7cad3989e1371a8e4587c74fa8f0
Gadenaboi-Sandeep <gadenaboisandeep2@gmail.com>
2026-07-09 11:14:50 +0530

    Done with Arrays

--- Sandeep/Arrays/CopyArray.java
+++ Sandeep/Arrays/CopyArray.java
@@ -4,9 +4,13 @@ public class CopyArray {
     public static void main(String[] args) {
         int[] arr={10,20,30,40,50,60,70,80,90,100};
 
-        int[] cp=arr;
+        int[] cp=new int[arr.length];
         
-
-        System.out.println(cp[0]);
+        for(int i=0;i<cp.length;i++){
+            cp[i]=arr[i];
+        }
+        for(int i:cp){
+        System.out.print(i+" ");
+        }
     }
 }
