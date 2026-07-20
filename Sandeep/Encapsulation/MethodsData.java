ebfca089b750ee5e2664c2b0462186e2f6f2ff28
Gadenaboi-Sandeep <gadenaboisandeep2@gmail.com>
2026-07-20 14:01:15 +0530

    Long gap

--- Sandeep/Encapsulation/MethodsData.java
+++ Sandeep/Encapsulation/MethodsData.java
@@ -1,23 +1,36 @@
 package Sandeep.Encapsulation;
 
 class Details{
-    private String name="Sandeep";
-    private int age=22;
+    private String name;
+    private int age;
 
-    void getName(){
-        System.out.println(name);
+    public Details(){
+        age=20;
+        name="Sandy";
     }
-    void getAge(){
-        System.out.println(age);
+    public Details(int a,String n){
+        age=a;
+        name=n;
     }
 
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public int getAge() {
+        return age;
+    }
+    public void setAge(int age) {
+        this.age = age;
+    }
 }
-
-
 public class MethodsData {
     public static void main(String[] args) {
         Details obj=new Details();
-        obj.getName();
-        obj.getAge();
+        Details obj1=new Details(21,"Sandeep");
+        System.out.println(obj.getName()+" : "+obj.getAge());
+        System.out.println(obj1.getName()+" : "+obj1.getAge());
     }
 }
