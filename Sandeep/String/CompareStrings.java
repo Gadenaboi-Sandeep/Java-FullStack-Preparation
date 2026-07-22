46fa802686a1c0ae7b8276ceed5c9683fb594105
Gadenaboi-Sandeep <gadenaboisandeep2@gmail.com>
2026-07-22 12:07:08 +0530

    Strings

---
+++ Sandeep/String/CompareStrings.java
@@ -0,0 +1,8 @@
+public class CompareStrings {
+    public static void main(String[] args) {
+        String s1="Sandeep";
+        String s2="Sandeep";
+        System.out.println(s1==s2);
+        System.out.println(s1.equals(s2));
+    }
+}
---
+++ Sandeep/String/CountWords.java
@@ -0,0 +1,8 @@
+public class CountWords {
+    public static void main(String[] args) {
+        String data="My name is Sandeep Gadenaboi";
+        String[] count=data.split(" ");
+        System.out.println("The words count is: "+count.length);
+        
+    }
+}
