e5afb619ee00da1e18a67eb8e258a588e8e80fe5
Chinna1222 <chinnareddygangam1222@gmail.com>
2026-08-08 10:30:35 +0530

    SpyNumber

---
+++ Chinna/HomeWork_Programs/SpyNum.java
@@ -0,0 +1,21 @@
+package Chinna.HomeWork_Programs;
+
+public class SpyNum {
+    public static void main(String[] args) {
+		int num = 123;
+		int sum = 0;
+		int prod = 1;
+		
+		while(num>0) {
+			int rem = num % 10;
+			sum = sum +rem;
+			prod = prod * rem;
+			num = num/10;
+		}
+		if(sum==prod) {
+			System.out.println("Spy number");
+		}else {
+			System.out.println("It is not spy number");
+		}
+	}
+}
