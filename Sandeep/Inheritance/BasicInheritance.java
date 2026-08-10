ef28e19a1b02db421e96e7bea217853ad4d44a57
Gadenaboi-Sandeep <gadenaboisandeep2@gmail.com>
2026-08-10 19:53:45 +0530

    Oops

---
+++ Sandeep/Inheritance/BasicInheritance.java
@@ -0,0 +1,10 @@
+package Sandeep.Inheritance;
+
+public class BasicInheritance {
+    public static void main(String[] args) {
+        AdvCal obj=new AdvCal();
+
+        int r1=obj.add(2,3);
+        System.out.println(r1);
+    }
+}
---
+++ Sandeep/Inheritance/Cal.java
@@ -0,0 +1,10 @@
+package Sandeep.Inheritance;
+
+public class Cal {
+    int add(int a,int b){
+        return a+b;
+    }
+    int sub(int a,int b){
+        return a-b;
+    }
+}
